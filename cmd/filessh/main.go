bb02535074043662aee75dd5cfac00f4db45fc8f
LSN0WM4N <rodriguezrondafrancis@gmail.com>
2026-09-21 11:09:12 +0200

    Error on Windows added

--- cmd/filessh/main.go
+++ cmd/filessh/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"runtime"
 
 	tea "charm.land/bubbletea/v2"
 	cli "github.com/LSN0WM4N/filessh/pkg/cli"
@@ -21,7 +22,18 @@ func main() {
 	cli.SetForceBackward(*forceBackward)
 
 	// Create the explorer
-	exp := explorer.New("/home/sn0wm4n")
+	homeDir := os.Getenv("HOME")
+
+	if homeDir == "" || runtime.GOOS == "windows" {
+		fmt.Printf(
+			"It seems like you are not using the best OS in the world.\n" +
+				"I am so sorry but i still working in linux main version, be patient. There\n" +
+				"will be a windows version soon. (or not).",
+		)
+		os.Exit(1)
+	}
+
+	exp := explorer.New(homeDir)
 	p := tea.NewProgram(cli.NewModel(exp))
 
 	if _, err := p.Run(); err != nil {
